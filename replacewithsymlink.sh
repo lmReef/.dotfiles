ea8b60831294f76029038c4fb012e163e1cd5a1b
lmReef <ReefMatson@gmail.com>
2024-11-21 12:51:23 +1300

    update script though it is no longer used

--- replacewithsymlink.sh
+++ replacewithsymlink.sh
@@ -6,13 +6,26 @@ if [[ -z $1 ]]; then
 elif [[ ! -e $1 ]]; then
     echo "no such file: $1"
     exit 1
+# elif [[ -d $1 ]]; then
+#     echo "$1 is a dir, to move all files use .../*"
+#     exit 1
 fi
+
 
 NEW_PATH="$(pwd -P)/$(basename $1)"
 
-mv $1 .
-echo "file moved to $NEW_PATH"
+echo "moving file(s)..."
+if [[ -d $1 ]]; then
+    shopt -s extglob
+    mv -v "$1/*" ./
+else
+    echo "false"
+    mv -v $1 .
+fi
 
-cd "$(dirname $1)"
-ln -nfs "$NEW_PATH"
-echo "symlink created at $1"
+echo "file(s) moved. updating symlinks..."
+./createallsymlinks.sh
+
+# cd "$(dirname $1)"
+# ln -nfs "$NEW_PATH"
+# echo "symlink created at $1"
