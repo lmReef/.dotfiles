0848cdca648b46dc793ddc405a2634fec7775759
lmReef <ReefMatson@gmail.com>
2025-01-04 13:26:42 +1300

    improve theme script

--- .local/bin/scripts/theme.sh
+++ .local/bin/scripts/theme.sh
@@ -1,12 +1,17 @@
 if [[ -z $1 || ! -f $1 ]]; then
-    wallpaper=~/Pictures/1440p/
+    wallpaper=~/Pictures/wallpapers/
 else
     wallpaper=$1
 fi
 
 wal -i $wallpaper > /dev/null
-
 wallpaper=$(jq .wallpaper ~/.cache/wal/colors.json)
+
+if [[ -f ~/.config/hypr/current_wallpaper.* ]]; then
+    rm ~/.config/hypr/current_wallpaper.*
+fi
+cp -lf $(echo $wallpaper | tr -d '"') "$HOME/.config/hypr/current_wallpaper.$(echo $wallpaper | tr -d '"' | sed 's/.*\.//g')"
+
 echo "preload = $wallpaper" | tr -d '"' | cat > ~/.config/hypr/hyprpaper.conf
 echo "wallpaper = , $wallpaper" | tr -d '"' | cat >> ~/.config/hypr/hyprpaper.conf
 
