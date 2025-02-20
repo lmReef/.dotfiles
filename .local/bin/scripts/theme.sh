7fbf52a38815cf135be35215d9ce764e29cf74cf
lmReef <ReefMatson@gmail.com>
2025-02-20 18:10:20 +1300

    fix waybar stylesheet gen

--- .local/bin/scripts/theme.sh
+++ .local/bin/scripts/theme.sh
@@ -8,9 +8,9 @@ fi
 echo "$wallpaper"
 
 if [[ -n $(echo "$wallpaper" | grep "/") ]]; then
-    wal -i "$wallpaper" > /dev/null
+    wal -i "$wallpaper" >/dev/null
 else
-    wal -i "$HOME/Pictures/wallpapers/$wallpaper" > /dev/null
+    wal -i "$HOME/Pictures/wallpapers/$wallpaper" >/dev/null
 fi
 wallpaper=$(jq .wallpaper "$HOME"/.cache/wal/colors.json)
 
@@ -21,22 +21,22 @@ if [[ -f $HOME/.config/hypr/current_wallpaper.* ]]; then
 fi
 cp -lf "$(echo "$wallpaper" | tr -d '"')" "$HOME/.config/hypr/current_wallpaper.$(echo "$wallpaper" | tr -d '"' | sed 's/.*\.//g')"
 
-echo "preload = $wallpaper" | tr -d '"' | cat > "$HOME"/.config/hypr/hyprpaper.conf
-echo "wallpaper = , $wallpaper" | tr -d '"' | cat >> "$HOME"/.config/hypr/hyprpaper.conf
+echo "preload = $wallpaper" | tr -d '"' | cat >"$HOME"/.config/hypr/hyprpaper.conf
+echo "wallpaper = , $wallpaper" | tr -d '"' | cat >>"$HOME"/.config/hypr/hyprpaper.conf
 
-hyprctl hyprpaper preload "$(echo "$wallpaper" | tr -d '"')" > /dev/null
-hyprctl hyprpaper wallpaper ",$(echo "$wallpaper" | tr -d '"')" > /dev/null
+hyprctl hyprpaper preload "$(echo "$wallpaper" | tr -d '"')" >/dev/null
+hyprctl hyprpaper wallpaper ",$(echo "$wallpaper" | tr -d '"')" >/dev/null
 
 # create waybar style.css from template
 waybar_stylesheet="$HOME/.config/waybar/style.css"
 template="$HOME/.config/waybar/template-style.css"
 wal_vars="$HOME/.cache/wal/colors"
-cat "$template" > "$waybar_stylesheet"
+cat "$template" >"$waybar_stylesheet"
 index=0
-for color in "$(cat "$wal_vars")"; do
-    sed "s/var(--color$index)/$color/" <<<"$(cat "$waybar_stylesheet")" > "$waybar_stylesheet"
-    ((index+=1))
+for color in $(cat "$wal_vars"); do
+    sed "s/var(--color$index)/$color/" <<<"$(cat "$waybar_stylesheet")" >"$waybar_stylesheet"
+    ((index += 1))
 done
-[[ -n "$(pgrep waybar)" ]] && killall -SIGUSR2 waybar &> /dev/null # refresh waybar config
+[[ -n "$(pgrep waybar)" ]] && killall -SIGUSR2 waybar &>/dev/null # refresh waybar config
 
 echo "Wallpaper set: $wallpaper"
