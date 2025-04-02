5042137726d5c3d31e31dee16f81c59df46f2199
lmReef <ReefMatson@gmail.com>
2025-04-02 19:15:50 +1300

    wip: initial dunst config

---
+++ .config/dunst/test_dunst.sh
@@ -0,0 +1,6 @@
+killall dunst
+dunst &
+
+dunstify -u critical "Error" "Something went wrong."
+dunstify -a "Test 1" "Information" "This is a test notification."
+dunstify -a "My App" "Information" "This is a test notification."
