e4abd04817979da6014f037911006ad41924dd68
lmReef <ReefMatson@proton.me>
2025-04-02 19:46:39 +1300

    tweak dunst stuff

--- .config/dunst/test_dunst.sh
+++ .config/dunst/test_dunst.sh
@@ -1,6 +1,6 @@
 killall dunst
 dunst &
 
-dunstify -u critical "Error" "Something went wrong."
-dunstify -a "Test 1" "Information" "This is a test notification."
-dunstify -a "My App" "Information" "This is a test notification."
+dunstify -u critical "Critical" "Something went wrong."
+dunstify -u normal "Normal" "This is a test notification."
+dunstify -u low "Low" "This is a test notification."
