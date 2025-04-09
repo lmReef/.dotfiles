fc782f1ee3e1c523adab4ac647e50bea4862cef7
lmReef <ReefMatson@proton.me>
2025-04-09 17:10:10 +1200

    remove pgrep focus

--- .local/bin/scripts/hyprland_focus_open.sh
+++ .local/bin/scripts/hyprland_focus_open.sh
@@ -1,13 +1,13 @@
 class_exists="$(hyprctl --instance 0 clients | grep "class: $1")"
 title_exists="$(hyprctl --instance 0 clients | grep "title: $1")"
-window_pid="$(pgrep "$1" | head -n1)"
+
+# echo "$class_exists"
+# echo "$title_exists"
 
 if [ -n "$class_exists" ]; then
     hyprctl --instance 0 dispatch focuswindow class:$1
 elif [ -n "$title_exists" ]; then
     hyprctl --instance 0 dispatch focuswindow title:$1
-elif [ -n "$window_pid" ]; then
-    hyprctl --instance 0 dispatch focuswindow pid:$window_pid
 else
     $1
 fi
