a271505753b970eaeb17285142d09e5a39d3bb50
lmReef <ReefMatson@proton.me>
2025-07-21 16:27:17 +1200

    update theme script

--- .local/bin/scripts/theme.sh
+++ .local/bin/scripts/theme.sh
@@ -1,25 +1,27 @@
-#!/bin/bash
+#!/bin/zsh
 
 if [[ -z "$1" ]]; then
-    # if fd -q "$1" ~/Pictures/wallpapers/; then
-    wallpaper=~/Pictures/wallpapers
+    wallpaper="$HOME/Pictures/wallpapers"
 elif [[ -f "$1" ]]; then
     wallpaper="$1"
 else
-    wallpaper="$(fd "$1" ~/Pictures/wallpapers/ | shuf -n 1)"
+    wallpaper="$(fd "$1" "$HOME/Pictures/wallpapers/" | shuf -n 1)"
 fi
 
 wal -i "$wallpaper"
-wallpaper="$(cat ~/.cache/wal/wal)"
+wallpaper="$(cat "$HOME/.cache/wal/wal")"
 
 # hyprpaper
-echo -e "preload = $wallpaper\nwallpaper = , $wallpaper" | tr -d '"' | cat >~/.config/hypr/hyprpaper.conf
+if [[ -z "$(pgrep hyprpaper)" ]]; then
+    hyprctl --instance 0 dispatch exec hyprpaper
+fi
+# echo -e "preload = $wallpaper\nwallpaper = , $wallpaper" | tr -d '"' | cat >"$HOME/.config/hypr/hyprpaper.conf"
 hyprctl --instance 0 hyprpaper reload ",$(echo "$wallpaper" | tr -d '"')" >/dev/null
 
 # create waybar style.css from template
-waybar_stylesheet=~/.config/waybar/style.css
-template=~/.config/waybar/template-style.css
-wal_vars=~/.cache/wal/colors
+waybar_stylesheet="$HOME/.config/waybar/style.css"
+template="$HOME/.config/waybar/template-style.css"
+wal_vars="$HOME/.cache/wal/colors"
 cat "$template" >"$waybar_stylesheet"
 index=0
 while read -r color; do
