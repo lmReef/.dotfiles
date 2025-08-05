8c45c546f44b6e54872fc6f5c8f538ff565e7433
lmReef <ReefMatson@proton.me>
2025-08-05 15:26:23 +1200

    tweak focus script

--- .local/bin/scripts/hyprland_focus_open.sh
+++ .local/bin/scripts/hyprland_focus_open.sh
@@ -1,7 +1,24 @@
-selector="$(hyprctl --instance 0 clients | grep -E "\s+\w+: $1" -m1 | sed -E "s/\s+(\w+): .+/\1/")"
+cinfo="$(hyprctl --instance 0 clients)"
 
-if [ "$selector" != "" ]; then
-    hyprctl --instance 0 dispatch focuswindow "$selector:$1"
+# pid based focus
+all_pids="$(echo "$cinfo" | awk '/pid:/ {print $2}')"
+matching_pid=""
+
+for pid in "${all_pids[@]}"; do
+    matching_pid="$(ps $pid | grep "$1" -m1 | awk '{split($0, x, "\\s"); print $1}')"
+done
+
+if [[ -n "$matching_pid" ]]; then
+    hyprctl --instance 0 dispatch focuswindow "pid:$matching_pid" 1>/dev/null
 else
     "$1" &
 fi
+
+# selector based focus
+# selector="$(echo "$cinfo" | grep -E "\s+\w+: $1" -m1 | sed -E "s/\s+(\w+): .+/\1/")"
+
+# if [ "$selector" != "" ]; then
+#     hyprctl --instance 0 dispatch focuswindow "$selector:$1.*"
+# else
+#     "$1" &
+# fi
