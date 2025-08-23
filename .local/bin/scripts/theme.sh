30539a5cf9926a606c7add04645ee24275ec9b84
lmReef <ReefMatson@proton.me>
2025-08-24 11:34:29 +1200

    update theme script

--- .local/bin/scripts/theme.sh
+++ .local/bin/scripts/theme.sh
@@ -1,22 +1,61 @@
 #!/bin/zsh
 
-if [[ -z "$1" ]]; then
-    wallpaper="$HOME/Pictures/wallpapers"
-elif [[ -f "$1" ]]; then
+
+if [[ -f "$1" ]]; then
     wallpaper="$1"
+elif [[ "$1" == "wallhaven" ]] && [[ -n "$WALLHAVEN_API_KEY" ]]; then
+    # TODO: the net side of this is probably better handled with js or py in another script
+    base_query="?apikey=$WALLHAVEN_API_KEY&atleast=2560x1440"
+    if [[ "$2" == "-s" ]]; then
+        query="q=$3"
+        json=$(curl -s "https://wallhaven.cc/api/v1/search${base_query}&$query")
+    else
+        # TODO: cache all /collections/ responses; split out to its own script and add to cron; currently only gets from page 1
+        # cached_list="$HOME/.cache/wallhaven/json"
+        # if [[ -f "$cached_list" ]] && [[ -n "$(cat "$cached_list")" ]]; then
+        #     echo "using cached json $cached_list"
+        #     json=$(cat "$cached_list")
+        # else
+        #     echo "no cached json, fetching"
+        #     json=$(curl -s "https://wallhaven.cc/api/v1/collections/lmReef/1866068${base_query}")
+        #     echo "$json" > "$cached_list"
+        # fi
+
+        # TODO: without -s flag still query but from collection items instead
+        json=$(curl -s "https://wallhaven.cc/api/v1/collections/lmReef/1866068${base_query}")
+    fi
+
+    wall_id=$(echo "$json" | jq -r '.data[].id' | shuf -n1)
+    detail=$(curl -s "https://wallhaven.cc/api/v1/w/${wall_id}?apikey=$WALLHAVEN_API_KEY")
+    url=$(echo "$detail" | jq -r '.data.path')
+    echo "$url"
+
+    filename=$(basename "$url")
+    wallhaven_dir="$HOME/.cache/wallhaven"
+    mkdir -p "$wallhaven_dir" &>/dev/null
+    wallpaper="$wallhaven_dir/$filename"
+
+    if [[ ! -f "$wallpaper" ]]; then
+        curl -s "$url" -o "$wallpaper"
+    fi
 else
-    wallpaper="$(fd "$1" "$HOME/Pictures/wallpapers/" | shuf -n 1)"
+    wallpaper="$(fd -d1 "$1" "$HOME/Pictures/wallpapers/" | shuf -n 1)"
 fi
+
+# copy wallpaper to persistent location for other process/configs to use w caching/reboots
+ext="${$(basename "$wallpaper")##*.}"
+current_wallpaper="$HOME/.cache/current_wallpaper.$ext"
+rm ~/.cache/current_wallpaper.*
+ln -f "$wallpaper" "$current_wallpaper"
 
 wal -i "$wallpaper"
-wallpaper="$(cat "$HOME/.cache/wal/wal")"
 
 # hyprpaper
-if [[ -z "$(pgrep hyprpaper)" ]]; then
+if ! pgrep hyprpaper &>/dev/null; then
     hyprctl --instance 0 dispatch exec hyprpaper
 fi
-# echo -e "preload = $wallpaper\nwallpaper = , $wallpaper" | tr -d '"' | cat >"$HOME/.config/hypr/hyprpaper.conf"
-hyprctl --instance 0 hyprpaper reload ",$(echo "$wallpaper" | tr -d '"')" >/dev/null
+echo -e "preload = $current_wallpaper\nwallpaper = , $current_wallpaper" | tr -d '"' | cat >"$HOME/.config/hypr/hyprpaper.conf"
+hyprctl --instance 0 hyprpaper reload ",$(echo "$current_wallpaper" | tr -d '"')"
 
 # create waybar style.css from template
 waybar_stylesheet="$HOME/.config/waybar/style.css"
@@ -27,7 +66,4 @@ index=0
 for color in $(cat "$wal_vars"); do
     sed "s/var(--color$index)/$color/" <<<"$(cat "$waybar_stylesheet")" >"$waybar_stylesheet"
     ((index += 1))
-done
-[[ -n "$(pgrep waybar)" ]] && killall -SIGUSR2 waybar &>/dev/null # refresh waybar config
-
-echo "Wallpaper set: $(basename $wallpaper)"
+done <"$wal_vars"
