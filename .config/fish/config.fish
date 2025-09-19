68a18ea047ea093826f18ad65950141a96e47876
lmReef <ReefMatson@proton.me>
2025-09-19 16:04:55 +1200

    fix fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,12 @@
 if status is-interactive
+    fish_add_path \
+        ~/.local/bin \
+        ~/.local/bin/scripts \
+        ~/.local/bin/rtg-tools \
+        ~/.cargo/bin \
+        ~/.config/tempus-app-manager/bin
+
+    /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
     mise activate fish | source
     zoxide init fish --cmd cd | source
     fzf --fish | source
